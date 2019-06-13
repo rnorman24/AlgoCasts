56d77f0075385dcca571b0098edbb90b0b51b489
unknown <nray24@gmail.com>
2019-06-13 13:40:29 -0600

    update

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,8 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  debugger;
   return str.split('').reduce((rev, char) => char + rev, '');
 }
+
+reverse('apple');
 
 module.exports = reverse;
 
