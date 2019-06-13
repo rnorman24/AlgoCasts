0d4d2790ec48f9dc552bcf85e4109ba540486a2a
unknown <nray24@gmail.com>
2019-06-13 17:11:03 -0600

    update

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -17,12 +17,18 @@
 // }
 
 function palindrome(str) {
-  const reversed = str
-    .split('')
-    .reverse()
-    .join('');
-
-  return str === reversed;
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return str === reversed;
+// }
