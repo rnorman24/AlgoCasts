47b46eb2d6abcf7e9d1e69fcb624e6d58e8f24aa
unknown <nray24@gmail.com>
2019-06-18 14:59:41 -0600

    update

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[chr]++;
+    } else {
+      charMap[char] = 1;
+    }
+    return charMap;
+  }
+}
 
 module.exports = maxChar;
