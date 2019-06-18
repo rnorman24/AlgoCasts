e4fba146032aa298a78ea0b253e540837c5fb835
unknown <nray24@gmail.com>
2019-06-18 15:05:42 -0600

    update

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,15 +7,24 @@
 
 function maxChar(str) {
   const charMap = {};
+  let max = 0;
+  let maxChar = '';
 
   for (let char of str) {
     if (charMap[char]) {
-      charMap[chr]++;
+      charMap[char]++;
     } else {
       charMap[char] = 1;
     }
-    return charMap;
   }
+    for (let char in charMap) {
+      if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
+      }
+    }
+  
+  return maxChar;
 }
 
 module.exports = maxChar;
